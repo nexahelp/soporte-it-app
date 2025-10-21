ca1c8fcd4e65590265969ec52f86eaf1f43c0181
nexahelp <nexahelp@outlook.com>
2025-10-21 18:24:12 -0500

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // <-- importante para rutas relativas en Vercel
+  base: './', // asegura rutas correctas en Vercel
 });
