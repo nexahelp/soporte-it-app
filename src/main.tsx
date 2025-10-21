df2d0fc372a8a57ffe458dc70a712f8c77880682
nexahelp <nexahelp@outlook.com>
2025-10-21 18:42:52 -0500

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './pages/App';
-import StatusPage from './pages/StatusPage';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./pages/App";
+import StatusPage from "./pages/StatusPage";
+import Dashboard from "./pages/Dashboard";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/estatus/:id" element={<StatusPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
